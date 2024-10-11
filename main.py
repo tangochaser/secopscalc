e9e96869481ede340279a6a4e8e9fbe23ae8655e
tangochaser <tnewshott@google.com>
2024-10-11 15:41:00 -0600

    Update main.py

--- main.py
+++ main.py
@@ -20,7 +20,13 @@ ingestFormatted = math.ceil(ingestAnnualTB)
 
 # This is outside the form
 with st.container(border=True):
-	st.write()
-	st.write("Selected License: ", license)
-	st.write("Annual Ingest in Tb: {:0,.0f}".format(ingestFormatted))
-	st.write("Calculated Annual Contract Value, List: ${:0,.0f}".format(cost).replace('$-','-$'))
+	if license == "SecOps Enterprise+" & cost < 400000: 
+		st.write("For SecOps Enterprise+ deals, the minimum post-discount price must be $400k or higher.")
+	if ingestAnnualTB > 100: 
+		#st.write()
+		st.write("Selected License: ", license)
+		st.write("Annual Ingest in Tb: {:0,.0f}".format(ingestFormatted))
+		st.write("Calculated Annual Contract Value, List: ${:0,.0f}".format(cost).replace('$-','-$'))
+		
+	else: 
+		st.write("This deal does not meet minimum deal size requirements.")
