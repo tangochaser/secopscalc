66e344829c74a1da2208c5a9d0378da7e98abe21
tangochaser <tnewshott@google.com>
2024-10-16 15:32:11 -0600

    Update main.py

--- main.py
+++ main.py
@@ -12,6 +12,8 @@ with st.form("my_form"):
 
 # secops = []
 # [metric, ingest, license, discount, customerSuccess]
+
+discount = discount / 100
 if metric == 'Gb/day': 
 	ingestAnnual = ingest * 365
 	ingestAnnualTB = ingestAnnual * .001
